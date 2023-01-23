30c66ee922b89d2b2e48339e2000f6725abb5593
Lofi9 <starymere@gmail.com>
2023-01-23 13:54:10 -0600

    build one

---
+++ Iris/target/debug/Iris.d
@@ -0,0 +1,1 @@
+/home/ino/MochiWeb/IrisBot/Iris/target/debug/Iris: /home/ino/MochiWeb/IrisBot/Iris/src/main.rs
---
+++ Iris/target/debug/deps/Iris-8fce8b7dfeaaec00.d
@@ -0,0 +1,5 @@
+/home/ino/MochiWeb/IrisBot/Iris/target/debug/deps/Iris-8fce8b7dfeaaec00: src/main.rs
+
+/home/ino/MochiWeb/IrisBot/Iris/target/debug/deps/Iris-8fce8b7dfeaaec00.d: src/main.rs
+
+src/main.rs:
