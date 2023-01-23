9f5447d35590271e3533ffd8cf35d5ef47c7771b
Lofi9 <starymere@gmail.com>
2023-01-23 14:15:32 -0600

    run this when you get home

--- Iris/src/main.rs
+++ Iris/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-}
+use serenity::{
+    client::Client,
+    framework::standard::StandardFramework,
+};
+
+#[tokio::main]
+async fn main() {
+    // Initialize the client
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .framework(StandardFramework::new().prefix("!").command
+
