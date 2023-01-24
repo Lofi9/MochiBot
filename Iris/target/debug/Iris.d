dbe361e9c1cffb1bca0fc4e2b6896b1c529412bb
Ino <91082100+Lofi9@users.noreply.github.com>
2023-01-24 11:04:29 -0600

    Delete Iris directory

--- Iris/target/debug/Iris.d
+++
@@ -1,1 +0,0 @@
-/home/ino/MochiWeb/IrisBot/Iris/target/debug/Iris: /home/ino/MochiWeb/IrisBot/Iris/src/main.rs
--- Iris/target/debug/deps/Iris-8fce8b7dfeaaec00.d
+++
@@ -1,5 +0,0 @@
-/home/ino/MochiWeb/IrisBot/Iris/target/debug/deps/Iris-8fce8b7dfeaaec00: src/main.rs
-
-/home/ino/MochiWeb/IrisBot/Iris/target/debug/deps/Iris-8fce8b7dfeaaec00.d: src/main.rs
-
-src/main.rs:
